95631dadb4e82e67c47623cff9b875cf1cbae88a
Balazs Kreith <kreith.balazs@gmail.com>
2021-11-16 10:03:53 +0200

    update schema changes

---
+++ scripts/merge_pr.py
@@ -0,0 +1,51 @@
+import argparse
+import json
+import requests
+import subprocess
+from git import Repo
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('-p','--project', help='The github project', required=True, type=str)
+    parser.add_argument('-r','--repo', help='The github repository', required=True, type=str)
+    parser.add_argument('-t','--token', help='The github token', required=True, type=str)
+    parser.add_argument('-b','--branch', help='The name of the new branch', required=True, type=str)
+    parser.add_argument('-cm','--commitmessage', help='The name of the new branch', required=True, type=str)
+    parser.add_argument('-ti','--title', help='The title of the new PR', required=True, type=str)
+    parser.add_argument('-bm','--bodymessage', help='The body message of the new PR', required=True, type=str)
+
+    args = parser.parse_args()
+    
+    repo = Repo("../.git")
+    main_branch_name = "main"  
+    new_branch_name = args.branch
+    current = repo.create_head(new_branch_name)
+    current.checkout()
+    
+    master = repo.heads.main
+    repo.git.pull('origin', master)
+    #creating file
+    if repo.index.diff(None) or repo.untracked_files:
+        repo.git.add(A=True)
+        repo.git.commit(m=args.commitmessage)
+        repo.git.push('--set-upstream', 'origin', current)
+        print('Changes detected')
+    else:
+        print('no changes detected, no pr is created')
+        exit(0)
+
+    from github import Github
+    g = Github(args.token)
+
+    repo = g.get_repo("ObserveRTC/website-3.0")
+    body = args.bodymessage
+
+    pr = repo.create_pull(title=args.title, body=body, head=new_branch_name, base=main_branch_name)
+    base = repo.get_branch(main_branch_name)
+    head = repo.get_branch(new_branch_name)
+
+    merge_to_master = repo.merge(main_branch_name, head.commit.sha, args.commitmessage)
+
+if __name__ == "__main__":
+    main()
